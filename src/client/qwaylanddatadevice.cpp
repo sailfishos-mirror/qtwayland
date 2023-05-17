c766dc477d695d9684a91ad6981f457df109f735
David Edmundson <davidedmundson@kde.org>
2023-05-17 09:06:03 +0300

    Client: Always populate mimedata in drags
    It's possible for clients to perform a drag and drop operation within
    their own client without any mimeData. A user can directly access the
    original drag.
    
    On wayland without any mimedata it's impossible for a client to accept a
    drag as the mechansim involved is to either select a given mimedata
    entry or an empty string. Within Qt we always accept the first format if
    we accept a drag.
    
    When dragging within our own window we also start a wayland drag so will
    receive a cancel event from the compositor if the compositor doesn't
    believe the client has accepted the drag.
    
    This patch provides a dummy mimedata entry so that something can be
    accepted.
    
    Fixes: QTBUG-112161
    Pick-to: 5.15
    Change-Id: I6309d82e20545e10ebdb9dafde7e13a5e3be5ff2
    Reviewed-by: Liang Qi <liang.qi@qt.io>
    (cherry picked from commit 32fedb6fa6579711b6cb192a2e3cfb7ad1264546)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5b05fec4f2016107bc21aaba4a3e74b6a8c82795)
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 145be6bbcd6157ae9fbfd84d1307f3c262ce9bc3)

--- src/client/qwaylanddatadevice.cpp
+++ src/client/qwaylanddatadevice.cpp
@@ -122,6 +122,12 @@ bool QWaylandDataDevice::startDrag(QMimeData *mimeData, QWaylandWindow *icon)
         return false;
     }
 
+    // dragging data without mimetypes is a legal operation in Qt terms
+    // but Wayland uses a mimetype to determine if a drag is accepted or not
+    // In this rare case, insert a placeholder
+    if (mimeData->formats().isEmpty())
+        mimeData->setData("application/x-qt-avoid-empty-placeholder", QByteArray("1"));
+
     m_dragSource.reset(new QWaylandDataSource(m_display->dndSelectionHandler(), mimeData));
     connect(m_dragSource.data(), &QWaylandDataSource::cancelled, this, &QWaylandDataDevice::dragSourceCancelled);
     connect(m_dragSource.data(), &QWaylandDataSource::targetChanged, this, &QWaylandDataDevice::dragSourceTargetChanged);
