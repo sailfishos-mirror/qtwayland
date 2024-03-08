ecd1e5c73f8f0c27bf914b4802a434dcb2f1473e
Liang Qi <liang.qi@qt.io>
2024-03-08 10:20:10 +0100

    client: use current modifiers in repeat keys
    This amends 35880c7e1021a379e3cdd4005edd53472b63856c .
    
    Fixes: QTBUG-123007
    Pick-to: 6.2 5.15
    Change-Id: I3b4e5d4a8304fd57558eec3897562895c294aadd
    Reviewed-by: David Edmundson <davidedmundson@kde.org>
    (cherry picked from commit 9ae6b38fef8a8ee7b3fbc0311216c8fa3b3e1d8f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d9c4088fa32cef1c9c22c432b6941a69758f1357)
    Reviewed-by: Liang Qi <liang.qi@qt.io>
    (cherry picked from commit a62d4425974c2ec794c0fde5ee4aeed0c395603c)

--- src/client/qwaylandinputdevice.cpp
+++ src/client/qwaylandinputdevice.cpp
@@ -70,11 +70,12 @@ QWaylandInputDevice::Keyboard::Keyboard(QWaylandInputDevice *p)
             return;
         }
         mRepeatTimer.setInterval(1000 / mRepeatRate);
-        handleKey(mRepeatKey.time, QEvent::KeyRelease, mRepeatKey.key, mRepeatKey.modifiers,
-                  mRepeatKey.code, mRepeatKey.nativeVirtualKey, mRepeatKey.nativeModifiers,
+        Qt::KeyboardModifiers modifiers = this->modifiers();
+        handleKey(mRepeatKey.time, QEvent::KeyRelease, mRepeatKey.key, modifiers,
+                  mRepeatKey.code, mRepeatKey.nativeVirtualKey, this->mNativeModifiers,
                   mRepeatKey.text, true);
-        handleKey(mRepeatKey.time, QEvent::KeyPress, mRepeatKey.key, mRepeatKey.modifiers,
-                  mRepeatKey.code, mRepeatKey.nativeVirtualKey, mRepeatKey.nativeModifiers,
+        handleKey(mRepeatKey.time, QEvent::KeyPress, mRepeatKey.key, modifiers,
+                  mRepeatKey.code, mRepeatKey.nativeVirtualKey, this->mNativeModifiers,
                   mRepeatKey.text, true);
     });
 }
@@ -1314,8 +1315,6 @@ void QWaylandInputDevice::Keyboard::keyboard_key(uint32_t serial, uint32_t time,
             mRepeatKey.code = code;
             mRepeatKey.time = time;
             mRepeatKey.text = text;
-            mRepeatKey.modifiers = modifiers;
-            mRepeatKey.nativeModifiers = mNativeModifiers;
             mRepeatKey.nativeVirtualKey = sym;
             mRepeatTimer.setInterval(mRepeatDelay);
             mRepeatTimer.start();
