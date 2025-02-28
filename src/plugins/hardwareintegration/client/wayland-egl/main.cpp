2122b623a78f5c2217b4e72cc36ce06eb0ed8dfb
David Redondo <qt@david-redondo.de>
2025-02-28 12:14:06 +0100

    Move client hardware integration sources into the plugin folder
    Change-Id: I49629ce1d55d8762ab30c33062fb78b05fdb05d4
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/plugins/hardwareintegration/client/wayland-egl/main.cpp
+++ src/plugins/hardwareintegration/client/wayland-egl/main.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include <QtWaylandClient/private/qwaylandclientbufferintegrationplugin_p.h>
-#include <QtWaylandEglClientHwIntegration/private/qwaylandeglclientbufferintegration_p.h>
+#include "qwaylandeglclientbufferintegration_p.h"
 
 QT_BEGIN_NAMESPACE
 
