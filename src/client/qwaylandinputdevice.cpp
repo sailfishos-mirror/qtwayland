be62083127d2deaca9c6178b9b18a6ce3214c3e9
YAMAMOTO Atsushi <atsushi.yamamoto@signal-slot.co.jp>
2025-04-24 17:09:25 +0900

    Fix build error when tablet support is disabled
    When Qt is configured with -no-feature-tabletevent, QtWayland fails to
    build because code referencing QWaylandInputDevice::mTabletSeat is still
    compiled, even though mTabletSeat is only available when tablet event
    support is enabled.
    
    Pick-to: 6.9 6.8
    Change-Id: Iad82ea47cfc64040772429f469d9104b75490891
    Reviewed-by: Liang Qi <liang.qi@qt.io>
    Reviewed-by: Tasuku Suzuki <tasuku.suzuki@signal-slot.co.jp>

--- src/client/qwaylandinputdevice.cpp
+++ src/client/qwaylandinputdevice.cpp
@@ -641,8 +641,10 @@ void QWaylandInputDevice::setCursor(const QCursor *cursor, const QSharedPointer<
     if (mPointer)
         mPointer->updateCursor();
 
+#if QT_CONFIG(tabletevent)
     if (mTabletSeat)
         mTabletSeat->updateCursor();
+#endif
 }
 #endif
 
